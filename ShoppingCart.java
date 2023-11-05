65c561af41622169e2edfa68210d4cde16c1e9a5
edgarC <128599934+Hyeonnnnnn@users.noreply.github.com>
2023-11-05 16:16:57 +0800

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -9,44 +9,21 @@ public class ShoppingCart {
             System.out.println("3. Replace an item (Update).");
             System.out.println("4. Delete an item (Delete).");
             System.out.println("5. Search an item (Search).");
-            System.out.println("6. Exit");
+            System.out.println("6. Clear cart");
+            System.out.println("7. Exit");
             System.out.print("\nChoose an option: ");
 
             int choice = myCart.scanner.nextInt();
             myCart.scanner.nextLine();
 
             switch (choice) {
-                case 1 -> {
-                    System.out.print("\nEnter item name: ");
-                    String name = myCart.scanner.nextLine();
-                    System.out.print("Enter item price: P");
-                    double price = myCart.scanner.nextDouble();
-                    System.out.print("Enter item quantity: ");
-                    int quantity = myCart.scanner.nextInt();
-                    myCart.addToCart(new Item(name, price, quantity));
-                    System.out.println(name + " has been successfully added in your cart.");
-                }
+                case 1 -> myCart.addToCart();
                 case 2 -> myCart.viewCart();
-                case 3 -> {
-                    if (!myCart.cart.isEmpty()) {
-                        System.out.print("\nEnter the name of the item you want to update: ");
-                        String oldName = myCart.scanner.nextLine();
-                        System.out.print("Enter new item name: ");
-                        String newName = myCart.scanner.nextLine();
-                        System.out.print("Enter new item price: P");
-                        double newPrice = myCart.scanner.nextDouble();
-                        System.out.print("Enter new item quantity: ");
-                        int newQuantity = myCart.scanner.nextInt();
-                        myCart.replaceItem(oldName, new Item(newName, newPrice, newQuantity));
-                    } else {
-                        System.out.println("\nThere's nothing to replace since your cart is empty.");
-                    }
-                }
+                case 3 -> myCart.replaceItem();
                 case 4 -> myCart.removeItem();
-                case 5 -> {
-                    myCart.searchCart();
-                }
-                case 6 -> System.exit(0);
+                case 5 -> myCart.searchCart();
+                case 6 -> myCart.clearCart();
+                case 7 -> System.exit(0);
             }
         }
     }
